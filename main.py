2cd0f5ccfab21ce5007c21af72dd85db0ac12ee8
yongzx <zhengxin.yong@minerva.kgi.edu>
2017-09-03 11:15:02 -0700

    Without Trial

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import heapq
 import random
 
 class TernaryTree:
@@ -57,12 +56,12 @@ class Time:
 	def __lt__(self, other):
 		return self.time < other.time
 
-class Schedule(TernaryTree):
+class Slots(TernaryTree):
 
 	class Slot:
-		def __init__(self, start, end, name = None, energy_level = None):
-			self.start = start
-			self.end = end
+		def __init__(self, start_t, end_t, name = None, energy_level = None):
+			self.start_t = start_t
+			self.end_t = end_t
 			self.name = name
 			self.energy_level = energy_level
 
@@ -127,11 +126,11 @@ class Schedule(TernaryTree):
 					slow = slow.next
 					fast = fast.next.next
 				
-				start = slow.next
+				start_t = slow.next
 				slow.next = None
 				
 				left = shuffle_helper(linked_list)
-				right = shuffle_helper(start)
+				right = shuffle_helper(start_t)
 				
 				head = merge(left, right)
 				return head
@@ -160,17 +159,37 @@ class Schedule(TernaryTree):
 
 		return shuffle_helper(linked_list)
 
+	def get_slot(self):
+		start = self.right.next
+		while start:
+			yield start.data
+			start = start.next
+
+		start = self.mid.next
+		while start:
+			yield start.data
+			start = start.next
+
+		start = self.left.next
+		while start:
+			yield start.data
+			start = start.next
+
 """
-S = Schedule()
+S = Slots()
 S.initialize_slots([])
+
 tmp_left = S.left
 tmp_mid = S.mid
 tmp_right= S.right
 for tmp in (tmp_left, tmp_mid, tmp_right):
 	while tmp:
-		print(tmp.data.start.hr if isinstance(tmp.data, Schedule.Slot) else tmp.data)
+		print(tmp.data.start.hr if isinstance(tmp.data, Slots.Slot) else tmp.data)
 		tmp = tmp.next
 	print("-----------------")
+
+for s in S.get_slot():
+	print(s.data.energy_level, s.data.start_t.hr, s.data.end_t.hr)
 """
 
 from bintrees import RBTree
@@ -186,19 +205,115 @@ class Tasks(TernaryTree):
 	def __str__(self):
 		return "{0} \n{1} \n{2} ".format(self.left.__repr__(), self.mid.__repr__(), self.right.__repr__())
 
-	def get_task_from_user(self, name, priority, duration_in_hr, deadline):
+	def get_task_from_user(self, name, priority, duration_in_min, deadline):
 		if priority == 3:
-			self.right[deadline] = [name, duration_in_hr]
+			self.right[(deadline,name)] = duration_in_min
 		elif priority == 2:
-			self.mid[deadline] = [name, duration_in_hr]
+			self.mid[(deadline,name)] = duration_in_min
 		elif priority == 1:
-			self.left[deadline] = [name, duration_in_hr]
+			self.left[(deadline,name)] = duration_in_min
 
+	def get_task(self):
+		iterator = self.right.iter_items(reverse=True)
+		for task in iterator:
+			yield task
 
+		iterator = self.mid.iter_items(reverse=True)
+		for task in iterator:
+			yield task
+
+		iterator = self.left.iter_items(reverse=True)
+		for task in iterator:
+			yield task
+
+	def update_task(self, task_storage =[]):
+		for task in task_storage:
+			deadline = task[0][0]
+			name = task[0][1]
+
+			if (deadline, name) in self.right:
+				self.right.pop((deadline,name))
+				if deadline-1 > 0:
+					self.right[(deadline-1, name)] = task[1] - task[1]/deadline
+
+			elif (deadline, name) in self.mid:
+				self.mid.pop((deadline, name))
+				if deadline-1 > 0:
+					self.mid[(deadline-1, name)] = task[1] - task[1]/deadline 
+
+			else:
+				self.left.pop((deadline, name))
+				if deadline-1 > 0:
+					self.left[(deadline-1, name)] = task[1] - task[1]/deadline 
+
+	def update_unfinished_task(self, task, duration):
+		deadline = task[0][0]
+		name = task[0][1]
+		if (deadline, name) in self.right:
+			self.right.pop((deadline,name))
+			if deadline - 1 > 0:
+				self.right[(deadline-1, name)] = task[1] - duration
+
+		elif (deadline, name) in self.mid:
+			self.mid.pop((deadline,name))
+			if deadline - 1 > 0:
+				self.mid[(deadline-1, name)] = task[1] - duration
+
+		elif (deadline, name) in self.left:
+			self.left.pop((deadline,name))
+			if deadline - 1 > 0:
+				self.left[(deadline-1, name)] = task[1] - duration
+
+"""
 T = Tasks()
 T.get_task_from_user("Essay", priority = 3, duration_in_hr = 3.5, deadline = 2)
 T.get_task_from_user("Event", priority = 3, duration_in_hr = 8, deadline = 4)
 T.get_task_from_user("Assigned Reading", priority = 3, duration_in_hr = 10, deadline = 5)
 T.get_task_from_user("Reading", priority = 2, duration_in_hr = 10, deadline = 20)
-
 print(T)
+"""
+
+class Schedule:
+	def __init__(self):
+		self.schedule = []
+
+	def create_schedule(self):
+		S = Slots()
+		S.initialize_slots()
+
+		T = Tasks()
+		T.get_task_from_user("Essay", priority = 3, duration_in_hr = 3.5, deadline = 2)
+		T.get_task_from_user("Event", priority = 3, duration_in_hr = 8, deadline = 4)
+		T.get_task_from_user("Assigned Reading", priority = 3, duration_in_hr = 10, deadline = 5)
+		T.get_task_from_user("Reading", priority = 2, duration_in_hr = 10, deadline = 20)
+
+		task_storage = []
+		curr_t, duration = 0, 0
+		for s in S.get_slot():
+			for t in T.get_task():
+				deadline = task[0][0] if not curr_t else curr_t[0][0]
+				name = task[0][1] if not curr_t else curr_t[0][1]
+				duration = task[1]/deadline if not duration else duration
+
+				if s.end_t - s.start_t == duration:
+					self.schedule.append((s.start_t, s.end_t, name))
+					task_storage.append(t)
+					curr_t, duration = 0, 0
+					break
+
+				elif s.end_t - s.start_t > duration:
+					self.schedule.append(s.start_t, s.start_t + duration, name)
+					task_storage.append(t)
+					curr_t, duration = 0, 0
+					s.start_t += expected_duration/deadline
+
+				else:
+					self.schedule.append((s.start_t, s.end_t, name))
+					duration -= (s.end_t - s.start_t)
+					curr_t = t
+					break
+
+		T.update_task(task_storage)
+		if curr_t:
+			T.update_unfinished_task(curr_t, duration)
+		return self.schedule
