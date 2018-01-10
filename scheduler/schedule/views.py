998a13d1123337ab2b4cf1405d03b5c280b083c3
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-01-10 07:28:25 -0800

    Add Index Page

--- scheduler/schedule/views.py
+++ scheduler/schedule/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import Task
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site.
+    Shows the schedule.
+    """
+    # generate counts of tasks
+    num_tasks = Task.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_tasks':num_tasks},
+    )
